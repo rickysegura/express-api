1bc3cff2294e20f41ab81e75998db7833397d38f
Rico The Dev <ricothedev@gmail.com>
2022-11-30 02:42:59 -0800

    Database Connection Est.
    Attempting to connect to the MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+/*
+ Development build...
+ Developer: Ricky Segura
+ Note: I can't believe I figured out how to configure a backend.
+*/
+
+// Init
 const express = require("express");
+const mongoose = require("mongoose")
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -6,6 +14,12 @@ const port = process.env.PORT || 3333;
 app.use(express.json());
 app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
+
+// Connect to database
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
+const db = mongoose.connection
+db.on("error", (error) => console.error(error))
+db.once("open", () => console.log("Connected to Database"))
 
 app.get("/", async (req, res) => {
   res.json({ message: "If this changes...it works...." });
